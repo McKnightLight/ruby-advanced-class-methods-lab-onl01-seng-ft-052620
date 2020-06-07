01284548c488f3bed0d2d6ea21dc772a97f1daff
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-07 00:13:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,6 +51,7 @@ class Song
     artist_name, song_name = file_name.split(" - ")
     song = self.new_by_name(song_name.gsub(".mp3", ""))
     song.artist_name = artist_name 
+    song
   end
   
 end
